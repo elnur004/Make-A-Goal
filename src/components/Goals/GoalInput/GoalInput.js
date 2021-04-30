45938be242b1bb195c553031f155ecc7f72ab44b
elnur004 <lnur004@gmail.com>
2021-05-01 02:06:52 +0400

    created styled component which used in same file

--- src/components/Goals/GoalInput/GoalInput.js
+++ src/components/Goals/GoalInput/GoalInput.js
@@ -1,7 +1,41 @@
 import { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './GoalInput.css';
+import styled from 'styled-components';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid label {
+    color: #ee3141;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: #ee5b6767;
+  }
+`;
 
 const GoalInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -23,10 +57,10 @@ const GoalInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid && 'invalid'}`}>
+      <FormControl className={!isValid && 'invalid'}>
         <label>Make A Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
